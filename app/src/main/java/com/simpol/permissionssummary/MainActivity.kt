d22b9d61339dbc2e1adc8b6efa774ef326a6a8f8
SimpolCo <Simpol.main@gmail.com>
2025-08-15 12:47:25 +0300

    bug fix: reload on app switch

--- app/src/main/java/com/simpol/permissionssummary/MainActivity.kt
+++ app/src/main/java/com/simpol/permissionssummary/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.simpol.permissionssummary.ui.theme.PermissionsSummaryTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,15 +16,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // FIRST
         setContent {
             PermissionsSummaryTheme {
                 Surface(
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppListScreen()
+                    val viewModel: AppListViewModel = viewModel()
+                    AppListScreen(viewModel = viewModel)
                 }
             }
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onResume() {
+        super.onResume()
+        val vm = ViewModelProvider(this)[AppListViewModel::class.java]
+        vm.loadPermissionGroups()
+    }
+
 }
+
